fa272bf06559cdc8bf47e51d03926a10d1e484f4
debjohnson33 <debjohnson_11@yahoo.com>
2019-02-08 20:03:36 -0600

    Added test for link creation

---
+++ test/graphql/mutations/create_link_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class Mutations::CreateLinkTest < ActiveSupport::TestCase
+  def perform(user: nil, **args)
+    Mutations::CreateLink.new(object: nil, context: {}).resolve(args)
+  end
+
+  test 'create a new link' do
+    user = create :user
+
+    link = perform(
+      url: 'http://example.com',
+      description: 'description',
+      user: user
+    )
+
+    assert link.persisted?
+    assert_equal link.description, 'description'
+    assert_equal link.url, 'http://example.com'
+    assert_equal link.user, user
+  end
+end
